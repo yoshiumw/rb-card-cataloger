400291bb84d55cb04c68afee7c32fe8741761844
unknown <wongyoshium@gmail.com>
2026-09-12 15:04:57 -0700

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/rb-card-cataloger/',
   server: {
     host: "0.0.0.0",
     port: 3000,
